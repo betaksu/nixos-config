afed600f2e5a100720af98e1500ed27cd83d9a50
韶光 <admin@gloryouth.com>
2025-12-07 23:15:15 +0800

    Update tuned service configuration for VPS performance profile
    - Changed the configuration to set the active profile for the `tuned` service directly in the `environment.etc` path, enhancing clarity and maintainability.
    - This update ensures that the `virtual-guest` profile is correctly applied for optimized performance in VPS environments.

--- server/vps/profiles/performance/generic.nix
+++ server/vps/profiles/performance/generic.nix
@@ -1,5 +1,6 @@
 {
   services.tuned.enable = true;
   # 针对 VPS 推荐 'virtual-guest' 或 'throughput-performance'
-  services.tuned.profile = "virtual-guest"; 
+
+  environment.etc."tuned/active_profile".text = "virtual-guest"; 
 }
